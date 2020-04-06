50a2afdc802bd94719ca2d7674a93f7e9502face
Greg <gkmarsella@gmail.com>
2020-04-06 06:55:23 -0400

    added html and css to blog and about

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'GregM',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 5 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 6 2020'
+    }
+]
 
 # render function takes request as first argument, and template name as second argument
 # third optional argument is 'context' which is used to pass information on to our template
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 # Create your views here.
