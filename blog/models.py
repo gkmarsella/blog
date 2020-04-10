a4ad1f4e20f72d818b0b89c8059c0a04e825ea1d
Greg <gkmarsella@gmail.com>
2020-04-09 21:22:00 -0400

    added POST to manage.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User #one to many relationship
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) #auto_now_add will not let you change the date, so we will import something
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
